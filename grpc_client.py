a295df7e2907bf3d082c314034fccdc831176933
Yarik <yarrnkt@mail.ru>
2026-08-17 13:35:58 +0300

    HTTPX

--- grpc_client.py
+++ grpc_client.py
@@ -9,4 +9,4 @@ stub = user_service_pb2_grpc.UserServiceStub(channel)
 
 # Отправляем запрос
 response = stub.GetUser(user_service_pb2.GetUserRequest(username="Alice"))
-print(response.message)  # Выведет: Привет, Alice!
+print(response.message)
